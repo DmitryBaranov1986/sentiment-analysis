262c82ababcb7b7e94c7c6b053a2803dfdcf23d1
DmitryBaranov1986 <satensera@gmail.com>
2018-11-21 18:42:06 +0300

    Create outputs

--- build_simple_model.py
+++ build_simple_model.py
@@ -1,4 +1,9 @@
 from nltk.corpus import movie_reviews
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
+from sklearn.pipeline import Pipeline
 
 negids = movie_reviews.fileids('neg')
 posids = movie_reviews.fileids('pos')
@@ -10,29 +15,26 @@ texts = negfeats + posfeats
 labels = [0] * len(negfeats) + [1] * len(posfeats)
 
 # Write out number of reviews
-with(open("output/simple_model_answer1.txt", "w")) as f:
-    f.write(len(texts))
+with open("output/simple_model_answer1.txt", "w") as f:
+    f.write(str(len(texts)))
 
 # Write out positive reviews ratio
-with(open("output/simple_model_answer2.txt", "w")) as f:
-    f.write(float(len(posfeats))/len(texts))
-
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score
-from sklearn import metrics
-from sklearn.pipeline import Pipeline
+with open("output/simple_model_answer2.txt", "w") as f:
+    f.write(str(float(len(posfeats))/len(texts)))
 
 mean_accuracy = cross_val_score(
     Pipeline([("vectorizer", CountVectorizer()),("classifier", LogisticRegression())]),
     texts, labels, scoring=metrics.make_scorer(metrics.accuracy_score)).mean()
 
-with(open("output/simple_model_answer3.txt", "w")) as f:
-    f.write(mean_accuracy)
+with open("output/simple_model_answer3.txt", "w") as f:
+    f.write(str(mean_accuracy))
+
 
 mean_rocauc = cross_val_score(
     Pipeline([("vectorizer", CountVectorizer()),("classifier", LogisticRegression())]),
     texts, labels, scoring='roc_auc').mean()
 
-with(open("output/simple_model_answer4.txt", "w")) as f:
-    f.write(mean_rocauc)
+with open("output/simple_model_answer4.txt", "w") as f:
+    f.write(str(mean_rocauc))
+
+
